ea8309fdd4d278d4615a1f4f53e4d673ded6ffbb
qinlinliu <qinlinliu619@gmail.com>
2026-01-21 06:39:43 +0800

    Fix GitHub Pages routing - add 404.html and update basename

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './App.css';
 
+// GitHub Pages SPA redirect handling
+// https://github.com/rafgraph/spa-github-pages
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, null,
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location))
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/qinlin2026">
       <App />
     </BrowserRouter>
   </React.StrictMode>
